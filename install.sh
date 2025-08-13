faf90e80ed72f15c98535429b275a36102a469d8
gandori <marcel.stiebing@web.de>
2025-08-13 09:55:37 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
+# This script is outdated and will be updated in the future.
+
 #!/bin/bash
 
-programs=(
-    i3-wm
-    i3blocks
-    rofi
-    picom
-    feh
-    powerline
-    lxappearance
-    caffeine
-    thunar
-    gimp
-    musikcube
-    docker.io
+#programs=(
+#    i3-wm
+#    i3blocks
+#    rofi
+#    picom
+#    feh
+#    powerline
+#    lxappearance
+#    caffeine
+#    thunar
+#    gimp
+#    musikcube
+#    docker.io
 #    docker-compose
-    docker-compose-v2
-    audacity
-    kitty
-)
-for programm in ${programs[@]}; do
-    sudo apt install $programm -y
-done
+#    docker-compose-v2
+#    audacity
+#    kitty
+#)
+#for programm in ${programs[@]}; do
+#    sudo apt install $programm -y
+#done
 
-files=(.profile .bash_aliases .bashrc)
-for file in ${files[@]}; do
-    sudo cp ./$file ~/
-done
+#files=(.profile .bash_aliases .bashrc)
+#for file in ${files[@]}; do
+#    sudo cp ./$file ~/
+#done
 
-sudo cp ./i3Custom.desktop /usr/share/xsessions/
+#sudo cp ./i3Custom.desktop /usr/share/xsessions/
 
